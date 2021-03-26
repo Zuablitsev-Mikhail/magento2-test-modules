7b7ac656e179ace9f84ab689a8af6b9714d5d5c3
Zuablitsev-Mikhail <mzuablitsev@gmail.com>
2021-03-26 11:09:20 +0300

    RelatedModule init

---
+++ code/Intexsoft/RelatedModule/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Mikhail-Ziablitsev All rights reserved.
+ * See COPYING.txt for license details.
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Intexsoft_RelatedModule', __DIR__);
+
